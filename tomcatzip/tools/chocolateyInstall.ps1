95726c7a7cb186ef1fe1a4134a3e5ec5caf8cc33
030 <r.l.vanbreukelen@gmail.com>
2014-06-01 15:40:05 +0200

    Automatic Package Updater Tomcatzip created

---
+++ tomcatzip/tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿$packageName = 'Apache Software Foundation'
+$version = '8'
+$updateNumber = '8'
+$serviceName = "tomcat${version}"
+$completeVersion = "{{PackageVersion}}"
+
+$baseUrl = "http://apache.hippo.nl/tomcat/tomcat-${version}/v${completeVersion}/bin/apache-tomcat-${completeVersion}-windows"
+$url = "${baseUrl}-x86.zip"
+$url64 = "${baseUrl}-x64.zip"
+$extractionPath = "C:/Program Files/$packageName"
+
+Install-ChocolateyZipPackage "$packageName" "$url" "$extractionPath" "$url64"
+Get-ChocolateyBins "$extractionPath"
+
+$catalinaHome = "${extractionPath}/apache-tomcat-${completeVersion}"
+
+Set-Variable CATALINA_HOME $catalinaHome
+$Env:CATALINA_HOME += $catalinaHome
+
+$createService = "${catalinaHome}/bin/service.bat"
+& $createService install $serviceName 
+net start $serviceName
