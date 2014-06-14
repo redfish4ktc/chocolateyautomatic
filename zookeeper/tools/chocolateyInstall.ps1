6d894e8e6364e1116d977e52a57916f09929ced6
030 <r.l.vanbreukelen@gmail.com>
2014-06-14 22:31:10 +0200

    Chocolatey Automatic Package Updater ZooKeeper created

---
+++ zookeeper/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'ZooKeeper'
+$authorVersion =  "{{PackageVersion}}"
+$installDir = "C:/$packageName"
+
+Install-ChocolateyZipPackage -packagename gzip-src -url "{{DownloadUrl}}" -unzipLocation (Join-Path $env:chocolateyPackageFolder tmp)
+Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder tmp\gzip-srcInstall) -destination "$installDir"
