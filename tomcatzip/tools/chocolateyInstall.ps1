6fca66c1d75b42e57a2f73ffcda276f77970d989
030 <r.l.vanbreukelen@gmail.com>
2014-06-15 01:27:38 +0200

    Automatic Package Updater tomcatzip created. 32bit and 64bit downloadUrls automatically created.

--- tomcatzip/tools/chocolateyInstall.ps1
+++ tomcatzip/tools/chocolateyInstall.ps1
@@ -1,18 +1,12 @@
 ﻿$packageName = 'Apache Software Foundation'
 $version = '8'
-$updateNumber = '8'
 $serviceName = "tomcat${version}"
-$completeVersion = "{{PackageVersion}}"
-
-$baseUrl = "http://apache.hippo.nl/tomcat/tomcat-${version}/v${completeVersion}/bin/apache-tomcat-${completeVersion}-windows"
-$url = "${baseUrl}-x86.zip"
-$url64 = "${baseUrl}-x64.zip"
 $extractionPath = "C:/Program Files/$packageName"
 
-Install-ChocolateyZipPackage "$packageName" "$url" "$extractionPath" "$url64"
+Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
 Get-ChocolateyBins "$extractionPath"
 
-$catalinaHome = "${extractionPath}/apache-tomcat-${completeVersion}"
+$catalinaHome = "${extractionPath}/apache-tomcat-{{PackageVersion}}"
 
 Set-Variable CATALINA_HOME $catalinaHome
 $Env:CATALINA_HOME += $catalinaHome
