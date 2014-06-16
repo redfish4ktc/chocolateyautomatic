b2ac6523def4c96cf0709ac064b4cfc5e35aee82
030 <r.l.vanbreukelen@gmail.com>
2014-06-16 23:39:29 +0200

    Output directory added

---
+++ _output/hadoop/2.4.0/tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'Hadoop'
+$authorVersion =  "2.4.0"
+$installDir = "C:/$packageName"
+
+Install-ChocolateyZipPackage -packagename gzip-src -url "http://ftp.nluug.nl/internet/apache/hadoop/common/hadoop-2.4.0/hadoop-2.4.0.tar.gz" -unzipLocation (Join-Path $env:chocolateyPackageFolder tmp)
+Get-ChocolateyUnzip -fileFullPath (Join-Path $env:chocolateyPackageFolder tmp\gzip-srcInstall) -destination "$installDir"
---
+++ _output/tomcatzip/8.0.8/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'Apache Software Foundation'
+$version = '8'
+$serviceName = "tomcat${version}"
+$extractionPath = "C:/Program Files/$packageName"
+
+Install-ChocolateyZipPackage "$packageName" "http://apache.hippo.nl/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8-windows-x86.zip" "$extractionPath" "http://apache.mirrors.hostinginnederland.nl/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8-windows-x64.zip"
+Get-ChocolateyBins "$extractionPath"
+
+$catalinaHome = "${extractionPath}/apache-tomcat-8.0.8"
+
+Set-Variable CATALINA_HOME $catalinaHome
+$Env:CATALINA_HOME += $catalinaHome
+
+$createService = "${catalinaHome}/bin/service.bat"
+& $createService install $serviceName 
+net start $serviceName
