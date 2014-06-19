321d925a562989fdbf4565978ad1ee2a5e9f8764
030 <r.l.vanbreukelen@gmail.com>
2014-06-19 21:19:37 +0200

    Work on #1. Implemented ChocolateyService function to install Tomcat Service

--- _output/tomcatzip/8.0.8/tools/chocolateyInstall.ps1
+++ _output/tomcatzip/8.0.8/tools/chocolateyInstall.ps1
@@ -3,48 +3,51 @@ $version = '8'
 $serviceName = "tomcat${version}"
 $extractionPath = "C:/Program Files/$packageName"
 $catalinaHome = "${extractionPath}/apache-tomcat-8.0.8"
-$port = '8080'
+$availablePort = '8080'
+$createServiceCommand = "${catalinaHome}/bin/service.bat"
+$completePackageName = 'Apache Tomcat 8.0.8'
 
-function installService() {
-    if (serviceExists) {
-        Write-Host "Apache Tomcat service will be installed"
-		$createService = "${catalinaHome}/bin/service.bat"
-		& $createService install $serviceName
-    }
-}
 
-function startService() {
-    if (portAvailable) {
-        Write-Host "Apache Tomcat service will be started"
-		start-service $serviceName
-    } else {
-		Write-ChocolateyFailure 'Apache Tomcat' "port $port is unavailable"
-		throw 
-    }
-}
+#function installService() {
+#    if (serviceExists) {
+#        Write-Host "Apache Tomcat service will be installed"
+#		$createService = "${catalinaHome}/bin/service.bat"
+#		& $createService install $serviceName
+#    }
+#}
 
-function portAvailable() {
-	Try {
-		$connection = (New-Object Net.Sockets.TcpClient)
-		$connection.Connect("127.0.0.1",$port)
-		return $FALSE
-	} Catch {
-		return $TRUE
-	}
-}
+#function startService() {
+#    if (portAvailable) {
+#        Write-Host "Apache Tomcat service will be started"
+#		start-service $serviceName
+#   } else {
+#		Write-ChocolateyFailure 'Apache Tomcat' "port $port is unavailable"
+#		throw 
+#    }
+#}
 
-function serviceExists() {
-	$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
-	if ($service.Length -gt 0) {
-        Write-Host "$serviceName service already exists and will be removed"
-		stop-service $serviceName
-		$service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
-		$service.delete()
-		return $TRUE
-    } else {
-		return $TRUE
-    }
-}
+#function portAvailable() {
+#	Try {
+#		$connection = (New-Object Net.Sockets.TcpClient)
+#		$connection.Connect("127.0.0.1",$port)
+#		return $FALSE
+#	} Catch {
+#		return $TRUE
+#	}
+#}
+
+#function serviceExists() {
+#	$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+#	if ($service.Length -gt 0) {
+#        Write-Host "$serviceName service already exists and will be removed"
+#		stop-service $serviceName
+#		$service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
+#		$service.delete()
+#		return $TRUE
+#   } else {
+#		return $TRUE
+#    }
+#}
 
 Install-ChocolateyZipPackage "$packageName" "http://apache.hippo.nl/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8-windows-x86.zip" "$extractionPath" "http://mirrors.supportex.net/apache/tomcat/tomcat-8/v8.0.8/bin/apache-tomcat-8.0.8-windows-x64.zip"
 
@@ -55,8 +58,9 @@ Write-Host "Setting Environment Variable CATALINA_HOME to $catalinaHome"
 Set-Variable CATALINA_HOME $catalinaHome
 Install-ChocolateyEnvironmentVariable 'CATALINA_HOME' $catalinaHome 'Machine'
 
-installService
+Install-ChocolateyService $packageName $serviceName $createServiceCommand $availablePort
+#installService
 
-startService
+#startService
 
-Write-ChocolateySuccess 'Apache Tomcat 8.0.8'
+Write-ChocolateySuccess $completePackageName
--- tomcatzip/tools/chocolateyInstall.ps1
+++ tomcatzip/tools/chocolateyInstall.ps1
@@ -2,49 +2,10 @@
 $version = '8'
 $serviceName = "tomcat${version}"
 $extractionPath = "C:/Program Files/$packageName"
-$catalinaHome = "${extractionPath}/apache-tomcat-{{PackageVersion}}"
-$port = '8080'
-
-function installService() {
-    if (serviceExists) {
-        Write-Host "Apache Tomcat service will be installed"
-		$createService = "${catalinaHome}/bin/service.bat"
-		& $createService install $serviceName
-    }
-}
-
-function startService() {
-    if (portAvailable) {
-        Write-Host "Apache Tomcat service will be started"
-		start-service $serviceName
-    } else {
-		Write-ChocolateyFailure 'Apache Tomcat' "port $port is unavailable"
-		throw 
-    }
-}
-
-function portAvailable() {
-	Try {
-		$connection = (New-Object Net.Sockets.TcpClient)
-		$connection.Connect("127.0.0.1",$port)
-		return $FALSE
-	} Catch {
-		return $TRUE
-	}
-}
-
-function serviceExists() {
-	$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
-	if ($service.Length -gt 0) {
-        Write-Host "$serviceName service already exists and will be removed"
-		stop-service $serviceName
-		$service = Get-WmiObject -Class Win32_Service -Filter "Name='$serviceName'"
-		$service.delete()
-		return $TRUE
-    } else {
-		return $TRUE
-    }
-}
+$catalinaHome = "${extractionPath}/apache-tomcat-8.0.8"
+$availablePort = '8080'
+$createServiceCommand = "${catalinaHome}/bin/service.bat"
+$completePackageName = 'Apache Tomcat 8.0.8'
 
 Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
 
@@ -55,8 +16,6 @@ Write-Host "Setting Environment Variable CATALINA_HOME to $catalinaHome"
 Set-Variable CATALINA_HOME $catalinaHome
 Install-ChocolateyEnvironmentVariable 'CATALINA_HOME' $catalinaHome 'Machine'
 
-installService
-
-startService
+Install-ChocolateyService $packageName $serviceName $createServiceCommand $availablePort
 
 Write-ChocolateySuccess 'Apache Tomcat {{PackageVersion}}'
