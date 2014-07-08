8b11ea2e79f479d6d73447e40e28d7b9e759bea7
030 <r.l.vanbreukelen@gmail.com>
2014-07-08 02:04:34 +0200

    (GH-4) Chocolatey Automatic Eclipse Package created

---
+++ eclipse/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$extractionPath = "C:/Program Files/Eclipse Foundation/{{PackageVersion}}"
+
+Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
+Get-ChocolateyBins "$extractionPath"
