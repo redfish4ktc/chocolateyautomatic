2094a71fe78cc1981cad3704ab7414068de45a60
030 <r.l.vanbreukelen@gmail.com>
2014-07-20 16:18:03 +0200

    (GH-8) Eclipse ADT set urls using ketarin, installZipPackage and logo

---
+++ eadt/tools/chocolateyInstall.ps1
@@ -0,0 +1,5 @@
+﻿$packageName = 'Eclipse ADT'
+$extractionPath = "C:/Program Files/$packageName"
+
+Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
+Write-ChocolateySuccess '$packageName'
