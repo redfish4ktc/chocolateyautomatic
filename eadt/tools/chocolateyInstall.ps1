2497869fb28e06e2878e6d4c0662542ee43b9bfb
030 <r.l.vanbreukelen@gmail.com>
2014-08-17 02:23:23 +0200

    (GH-10) install package before determining path executables

--- eadt/tools/chocolateyInstall.ps1
+++ eadt/tools/chocolateyInstall.ps1
@@ -1,9 +1,11 @@
 ﻿$packageName = 'Eclipse ADT'
 $extractionPath = "C:/Google"
+
+Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
+
 $sdkManager = (gci "${extractionPath}/*/SDK Manager.exe").FullName | sort -Descending | Select -first 1
 $eclipse = (gci "${extractionPath}/*/eclipse/eclipse.exe").FullName
 
-Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
 Install-ChocolateyDesktopLink $sdkManager
 Install-ChocolateyPinnedTaskBarItem $sdkManager
 Install-ChocolateyDesktopLink $eclipse
