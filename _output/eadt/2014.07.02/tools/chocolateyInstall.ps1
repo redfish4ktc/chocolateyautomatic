5e4ee4cec8016497feb3530539fd16acaa513c1b
030 <r.l.vanbreukelen@gmail.com>
2014-08-17 02:20:30 +0200

    (GH-10) install package before determining path executables

--- _output/eadt/2014.07.02/tools/chocolateyInstall.ps1
+++ _output/eadt/2014.07.02/tools/chocolateyInstall.ps1
@@ -1,15 +1,11 @@
 ﻿$packageName = 'Eclipse ADT'
 $extractionPath = "C:/Google"
 
-  if (!$extractionPath) {
-    Write-Host "$extractionPath directory does not exist and will be created"
-    New-Item -ItemType Directory -Path $extractionPath
-  }
+Install-ChocolateyZipPackage "$packageName" "http://dl.google.com/android/adt/adt-bundle-windows-x86-20140702.zip" "$extractionPath" "http://dl.google.com/android/adt/adt-bundle-windows-x86_64-20140702.zip"
 
 $sdkManager = (gci "${extractionPath}/*/SDK Manager.exe").FullName | sort -Descending | Select -first 1
 $eclipse = (gci "${extractionPath}/*/eclipse/eclipse.exe").FullName
 
-Install-ChocolateyZipPackage "$packageName" "http://dl.google.com/android/adt/adt-bundle-windows-x86-20140702.zip" "$extractionPath" "http://dl.google.com/android/adt/adt-bundle-windows-x86_64-20140702.zip"
 Install-ChocolateyDesktopLink $sdkManager
 Install-ChocolateyPinnedTaskBarItem $sdkManager
 Install-ChocolateyDesktopLink $eclipse
