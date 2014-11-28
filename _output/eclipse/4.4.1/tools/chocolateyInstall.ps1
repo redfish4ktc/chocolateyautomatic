652fb6e4a02791cd49c800171f610b35a36d8b1c
Administrator <admin@example.com>
2014-11-29 00:53:05 +0100

    (GH-15) eclipse version 4.4.1  * ketarin file moved  * fixed broken ketarin automatic update

---
+++ _output/eclipse/4.4.1/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'eclipse'
+$packageVersion = '4.4.1'
+$32BitUrl = 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/luna/SR1/eclipse-jee-luna-SR1-win32.zip'
+$64BitUrl = 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/luna/SR1/eclipse-jee-luna-SR1-win32-x86_64.zip'
+$extractionPath = "c:\Program Files\Eclipse Foundation\$packageVersion"
+
+Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$extractionPath" "$64BitUrl"
