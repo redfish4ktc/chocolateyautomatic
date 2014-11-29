97471faea034dd1023e592e4461c3bdaef876e27
Administrator <admin@example.com>
2014-11-29 01:03:45 +0100

    (GH-15) eclipse version 4.4.1  * ketarin file moved  * fixed broken ketarin automatic update

--- eclipse/tools/chocolateyInstall.ps1
+++ eclipse/tools/chocolateyInstall.ps1
@@ -1,4 +1,7 @@
-﻿$extractionPath = "C:/Program Files/Eclipse Foundation/{{PackageVersion}}"
+﻿$packageName = '{{PackageName}}'
+$packageVersion = '{{PackageVersion}}'
+$32BitUrl = '{{DownloadUrl}}'
+$64BitUrl = '{{DownloadUrlx64}}'
+$extractionPath = "c:\Program Files\Eclipse Foundation\$packageVersion"
 
-Install-ChocolateyZipPackage "$packageName" "{{DownloadUrl}}" "$extractionPath" "{{DownloadUrlx64}}"
-Get-ChocolateyBins "$extractionPath"
+Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$extractionPath" "$64BitUrl"
