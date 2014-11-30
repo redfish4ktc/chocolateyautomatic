6fe1d93d13e99e6f8bcec5aa543a121fb6200700
Administrator <030@users.noreply.github.com>
2014-11-30 21:50:11 +0100

    (GH-16) Uninstalling Eclipse by removing directory c:\Program Files\Eclipse Foundation
    c:\HashiCorp>choco uninstall eclipse
    Chocolatey (v0.9.8.28) is uninstalling eclipse...
    Uninstalling from folder C:\ProgramData\Chocolatey\lib\eclipse.4.4.1
    Uninstalling by removing directory c:\Program Files\Eclipse Foundation

---
+++ eclipse/tools/chocolateyUninstall.ps1
@@ -0,0 +1,6 @@
+﻿$extractionPath = "c:\Program Files\Eclipse Foundation"
+
+if(Test-Path -Path $extractionPath) {
+	Write-Host "Uninstalling by removing directory $extractionPath"
+	Remove-Item -Recurse -Force $extractionPath
+}
