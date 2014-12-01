588cb9c28f12e85b492637abd09e05e558524998
Administrator <030@users.noreply.github.com>
2014-12-01 01:13:32 +0100

    (GH-17) install eclipse in a custom directory by passing a parameter while installing

--- _output/eclipse/4.4.1/tools/chocolateyInstall.ps1
+++ _output/eclipse/4.4.1/tools/chocolateyInstall.ps1
@@ -1,7 +1,30 @@
-﻿$packageName = 'eclipse'
+﻿$arguments = @{};
+
+$packageParameters = $env:chocolateyPackageParameters;
+
+$packageName = 'eclipse'
 $packageVersion = '4.4.1'
 $32BitUrl = 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/luna/SR1/eclipse-jee-luna-SR1-win32.zip'
 $64BitUrl = 'http://eclipse.mirror.triple-it.nl/technology/epp/downloads/release/luna/SR1/eclipse-jee-luna-SR1-win32-x86_64.zip'
-$extractionPath = "c:\Program Files\Eclipse Foundation\$packageVersion"
+$extractionPath = "C:\Program Files\Eclipse Foundation\$packageVersion"
+
+if($packageParameters) {
+	$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?(.*)([`"'])?"
+	$PARAMATER_NAME_INDEX = 1
+	$VALUE_INDEX = 3
+	
+	if($packageParameters -match $MATCH_PATTERN ){
+		$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches 
+		$results.matches | % { 
+		$arguments.Add(
+			$_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
+			$_.Groups[$VALUE_INDEX].Value.Trim()) 
+		}
+	}     
+	
+	if($arguments.ContainsKey("ExtractionPath")) {
+		$extractionPath = $arguments["ExtractionPath"];
+	}
+}
 
 Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$extractionPath" "$64BitUrl"
--- eclipse/tools/chocolateyInstall.ps1
+++ eclipse/tools/chocolateyInstall.ps1
@@ -1,7 +1,30 @@
-﻿$packageName = '{{PackageName}}'
+﻿$arguments = @{};
+
+$packageParameters = $env:chocolateyPackageParameters;
+
+$packageName = '{{PackageName}}'
 $packageVersion = '{{PackageVersion}}'
 $32BitUrl = '{{DownloadUrl}}'
 $64BitUrl = '{{DownloadUrlx64}}'
-$extractionPath = "c:\Program Files\Eclipse Foundation\$packageVersion"
+$extractionPath = "C:\Program Files\Eclipse Foundation\$packageVersion"
+
+if($packageParameters) {
+	$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?(.*)([`"'])?"
+	$PARAMATER_NAME_INDEX = 1
+	$VALUE_INDEX = 3
+	
+	if($packageParameters -match $MATCH_PATTERN ){
+		$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches 
+		$results.matches | % { 
+		$arguments.Add(
+			$_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
+			$_.Groups[$VALUE_INDEX].Value.Trim()) 
+		}
+	}     
+	
+	if($arguments.ContainsKey("ExtractionPath")) {
+		$extractionPath = $arguments["ExtractionPath"];
+	}
+}
 
 Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$extractionPath" "$64BitUrl"
