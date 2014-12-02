a4c30251f05178d1b0bfc242a7c03e911ed7352f
Administrator <030@users.noreply.github.com>
2014-12-02 02:14:02 +0100

    (GH-15) Added description

--- eclipse/tools/chocolateyInstall.ps1
+++ eclipse/tools/chocolateyInstall.ps1
@@ -1,30 +1,11 @@
-﻿$arguments = @{};
-
-$packageParameters = $env:chocolateyPackageParameters;
-
-$packageName = '{{PackageName}}'
+﻿$packageName = '{{PackageName}}'
 $packageVersion = '{{PackageVersion}}'
 $32BitUrl = '{{DownloadUrl}}'
 $64BitUrl = '{{DownloadUrlx64}}'
-$extractionPath = "C:\Program Files\Eclipse Foundation\$packageVersion"
+$global:extractionPath = "C:\Program Files\Eclipse Foundation\$packageVersion"
 
-if($packageParameters) {
-	$MATCH_PATTERN = "/([a-zA-Z]+):([`"'])?(.*)([`"'])?"
-	$PARAMATER_NAME_INDEX = 1
-	$VALUE_INDEX = 3
-	
-	if($packageParameters -match $MATCH_PATTERN){
-		$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches 
-		$results.matches | % { 
-		$arguments.Add(
-			$_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
-			$_.Groups[$VALUE_INDEX].Value.Trim()) 
-		}
-	}     
-	
-	if($arguments.ContainsKey("ExtractionPath")) {
-		$extractionPath = $arguments["ExtractionPath"];
-	}
-}
+. "$PSScriptRoot\OverwriteParameters030.ps1"
 
-Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$extractionPath" "$64BitUrl"
+OverwriteParameters030
+
+Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$global:extractionPath" "$64BitUrl"
