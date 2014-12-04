1094e1b54629928a18bbede9b5e7a1aad7e30925
Administrator <030@users.noreply.github.com>
2014-12-04 17:32:48 +0100

    (GH-14) uninstall eadt

--- common/Uninstall-ChocolateyZipPackage030.ps1
+++ common/Uninstall-ChocolateyZipPackage030.ps1
@@ -2,6 +2,11 @@ function Uninstall-ChocolateyZipPackage030 {
 param(
   [string] $packageName
 )
+	if(!$packageName) {
+		Write-ChocolateyFailure "Uninstall-ChocolateyZipPackage030" "Missing PackageName input parameter."
+		return
+	}
+
 	ChildItem "$env:ChocolateyInstall\lib\${packageName}.*" -Recurse -Filter "${packageName}Install.zip.txt" | 
-	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1); Write-Host "Uninstalling by removing directory $installLocation"; if (($installLocation -match "${packageName}") -and (Test-Path -Path $installLocation)){Remove-Item -Recurse -Force $installLocation}}
+	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1); Write-Host "Uninstalling by removing directory $installLocation"; if (($installLocation -match '${packageName}|adt-bundle-windows-x86_64') -and (Test-Path -Path $installLocation)){Remove-Item -Recurse -Force $installLocation}}
 }
