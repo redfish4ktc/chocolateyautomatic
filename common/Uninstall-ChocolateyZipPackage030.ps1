80ec51b6ad3532032f74b10c2711ce511f188e11
Administrator <030@users.noreply.github.com>
2014-12-05 21:34:20 +0100

    (GH-18) fixed issue that it was not possible to lookup variable and match it

--- common/Uninstall-ChocolateyZipPackage030.ps1
+++ common/Uninstall-ChocolateyZipPackage030.ps1
@@ -6,7 +6,12 @@ param(
 		Write-ChocolateyFailure "Uninstall-ChocolateyZipPackage030" "Missing PackageName input parameter."
 		return
 	}
-
+	
 	ChildItem "$env:ChocolateyInstall\lib\${packageName}.*" -Recurse -Filter "${packageName}Install.zip.txt" | 
-	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1); Write-Host "Uninstalling by removing directory $installLocation"; if (($installLocation -match '${packageName}|adt-bundle-windows-x86_64') -and (Test-Path -Path $installLocation)){Remove-Item -Recurse -Force $installLocation}}
+	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1);
+		if (("$installLocation" -match "${packageName}|adt-bundle-windows-x86_64") -and (Test-Path -Path "$installLocation")) {
+			Write-Host "Uninstalling by removing directory $installLocation";
+			Remove-Item -Recurse -Force "$installLocation"
+		}
+	}
 }
