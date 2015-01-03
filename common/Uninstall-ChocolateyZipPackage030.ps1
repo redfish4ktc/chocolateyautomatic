362b5668a0049ac2a4f390ecd88dbfdd9cdec4e0
Administrator <030@users.noreply.github.com>
2015-01-03 19:36:46 +0100

    #22 built in uninstall support

--- common/Uninstall-ChocolateyZipPackage030.ps1
+++ common/Uninstall-ChocolateyZipPackage030.ps1
@@ -9,7 +9,7 @@ param(
 	
 	ChildItem "$env:ChocolateyInstall\lib\${packageName}.*" -Recurse -Filter "${packageName}Install.zip.txt" | 
 	ForEach-Object{ $installLocation = (Get-Content $_.FullName | Select-Object -First 1);
-		if (("$installLocation" -match "${packageName}|adt-bundle-windows-x86_64") -and (Test-Path -Path "$installLocation")) {
+		if (("$installLocation" -match "${packageName}|apache-tomcat") -and (Test-Path -Path "$installLocation")) {
 			Write-Host "Uninstalling by removing directory $installLocation";
 			Remove-Item -Recurse -Force "$installLocation"
 		}
