22e4da46b8bd575c46590fb59b58da5349b484de
Administrator <030@users.noreply.github.com>
2015-01-18 02:11:23 +0100

    #25 fixed incorrect regex

--- common/OverwriteParameters030.ps1
+++ common/OverwriteParameters030.ps1
@@ -3,12 +3,13 @@ function OverwriteParameters030 {
 	$packageParameters = $env:chocolateyPackageParameters;
 
 	if($packageParameters) {
-		$MATCH_PATTERN = "/([a-zA-Z]+)=([`"'])?(.*)([`"'])?"
+		$MATCH_PATTERN = "/([a-zA-Z]+)=(.*)(`")"
 		$PARAMATER_NAME_INDEX = 1
-		$VALUE_INDEX = 3
+		$VALUE_INDEX = 2
 		
 		if($packageParameters -match $MATCH_PATTERN){
 			$results = $packageParameters | Select-String $MATCH_PATTERN -AllMatches 
+			
 			$results.matches | % { 
 			$arguments.Add(
 				$_.Groups[$PARAMATER_NAME_INDEX].Value.Trim(),
@@ -18,6 +19,7 @@ function OverwriteParameters030 {
 		
 		if($arguments.ContainsKey("InstallLocation")) {
 			$global:installLocation = $arguments["InstallLocation"];
+			
 			Write-Host "Value variable installLocation changed to $global:installLocation"
 		}
 	}	
