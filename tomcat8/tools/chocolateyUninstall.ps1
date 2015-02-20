b7d4375bc2dd7a89b51fb9665bb1f6b633c2a1b7
Administrator <030@users.noreply.github.com>
2015-02-20 23:21:56 +0100

    removed tomcat8

--- tomcat8/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-﻿$packageName = '{{PackageName}}'
-
-if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-. "$PSScriptRoot\Install-Service.ps1"
-. "$PSScriptRoot\Uninstall-ZipPackage.ps1"
-
-Uninstall-Service "$packageName"
-
-Uninstall-ZipPackage "$packageName"
