a56b01a7c8c1766e00a41f8bf9b7f9eedd129925
Administrator <030@users.noreply.github.com>
2015-04-13 21:02:30 +0200

    eclipse nuspec updated

--- eclipse/tools/chocolateyInstall.ps1
+++ eclipse/tools/chocolateyInstall.ps1
@@ -9,9 +9,9 @@ $checksum64 = '{{Checksumx64}}'
 $checksumType64 = 'md5'
 
 if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
-. "$PSScriptRoot\OverwriteParameters030.ps1"
+. "$PSScriptRoot\OverwriteParameters.ps1"
 
-OverwriteParameters030
+OverwriteParameters
 
 Install-ChocolateyZipPackage "$packageName" "$32BitUrl" "$global:installLocation" "$64BitUrl" -checksum "$checksum" -checksumType "$checksumType" -checksum64 "$checksum64" -checksumType64 "$checksumType64"
 
