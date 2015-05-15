d824d62ecea9eb203f5061d865b3744bf3375bd8
Administrator <030@users.noreply.github.com>
2015-05-16 01:15:08 +0200

    [GH-27] wildfly ketarin and nuspec creared

---
+++ wildfly/tools/chocolateyUninstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = '{{PackageName}}'
+
+if(!$PSScriptRoot){ $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent }
+. "$PSScriptRoot\Install-Service.ps1"
+. "$PSScriptRoot\Uninstall-ZipPackage.ps1"
+
+Uninstall-Service "$packageName"
+
+Uninstall-ZipPackage "$packageName"
